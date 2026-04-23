5012ad180aa2b81c100e200ba942f60430f99abc
ola-iya <bancolejeandedieu@gmail.com>
2026-04-23 14:30:44 +0100

    rrrrrrrrrr

--- application.cpp
+++ application.cpp
@@ -1,163 +1,180 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-#include <iostream>
 #include "Shader.h"
+#include <iostream>
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void processInput(GLFWwindow* window);
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+void processInput(GLFWwindow *window);
 
 // settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
-
-int main(void)
-{
-    // glfw: initialize and configure
-    // ------------------------------
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+int main(void) {
+  // glfw: initialize and configure
+  // ------------------------------
+  glfwInit();
+  
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 #ifdef __APPLE__
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
 
-    // glfw window creation
-    // --------------------
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
-    if (window == NULL)
-    {
-        std::cout << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-    glfwMakeContextCurrent(window);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-    // glad: load all OpenGL function pointers
-    // ---------------------------------------
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        std::cout << "Failed to initialize GLAD" << std::endl;
-        return -1;
-    }
-    Shader shader1("shader.vs", "shader.fs"), shader2("shader.vs", "shader.fs2");
-
-    // set up vertex data (and buffer(s)) and configure vertex attributes
-    // ------------------------------------------------------------------
-    float firstTriangle[] = {
-        -0.5f,  0.0f, 0.0f, 1.0f, 0.0f, 0.0f, // left 
-         0.0f, -0.0f, 0.0f, 0.0f, 1.0f, 0.0f,  // right
-         0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f // top 
-    };
-    float secondTriangle[] = {
-        0.5f, -0.0f, 0.0f, 0.0f, 0.0f, 1.0f,  // left
-        0.0f, -0.0f, 0.0f, 0.0f, 1.0f, 0.0f, // right
-        0.0f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f   // top 
-    };
-    unsigned int VBOs[2], VAOs[2];
-    glGenVertexArrays(2, VAOs); // we can also generate multiple VAOs or buffers at the same time
-    glGenBuffers(2, VBOs);
-    // first triangle setup
-    // --------------------
-    glBindVertexArray(VAOs[0]);
-    glBindBuffer(GL_ARRAY_BUFFER, VBOs[0]);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(firstTriangle), firstTriangle, GL_STATIC_DRAW);
-
-    // le vertex pointer pour l'attribut aPos
-    //-------------------(location attribut, taille, --, --, stride, --)
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);	// Vertex attributes stay the same
-    glEnableVertexAttribArray(0);
-
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
-    // glBindVertexArray(0); // no need to unbind at all as we directly bind a different VAO the next few lines
-    // second triangle setup
-    // ---------------------
-    glBindVertexArray(VAOs[1]);	// note that we bind to a different VAO now
-    glBindBuffer(GL_ARRAY_BUFFER, VBOs[1]);	// and a different VBO
-    glBufferData(GL_ARRAY_BUFFER, sizeof(secondTriangle), secondTriangle, GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);	// Vertex attributes stay the same
-    glEnableVertexAttribArray(0);
-
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
-    // glBindVertexArray(0); // not really necessary as well, but beware of calls that could affect VAOs while this one is bound (like binding element buffer objects, or enabling/disabling vertex attributes)
-
-
-    // uncomment this call to draw in wireframe polygons.
-    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-
-    // render loop
-    // -----------
-    while (!glfwWindowShouldClose(window))
-    {
-        // input
-        // -----
-        processInput(window);
-
-        // render
-        // ------
-        glClearColor(0.15f, 0.15f, 0.15f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        // utilisation de "uniform" dans un shader GLSL
-        
-        //float timevalue = glfwGetTime();
-        //float greenValue = (sin(timevalue) / 2.0f)  + 0.5f; // on veut modifier la valeur en controlant la valeur de G dans RGBA
-        //float redValue = (cos(timevalue) / 2.0f) + 0.5f;
-        //float blueValue = (acos(timevalue) / 2.0f) + 0.5f;
-        //Notons que la localisation de la valeur "uniform" e requiert pas l'utilisation du shader
-        //int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
-        
-        shader1.use();
-        shader1.setFloat("offset", 0.1f);
-
-        // Notons également que la MAJ d'un "uniform" se fait apres utilisation du shader (glUseProgram)
-        //glUniform4f(vertexColorLocation, redValue, greenValue, blueValue, 1.0f);
-        // draw first triangle using the data from the first VAO
-        glBindVertexArray(VAOs[0]);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-        // then we draw the second triangle using the data from the second VAO
-        
-        shader2.use();
-        glBindVertexArray(VAOs[1]);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-
-        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
-        // -------------------------------------------------------------------------------
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-
-    // optional: de-allocate all resources once they've outlived their purpose:
-    // ------------------------------------------------------------------------
-    glDeleteVertexArrays(2, VAOs);
-    glDeleteBuffers(2, VBOs);
-
-    // glfw: terminate, clearing all previously allocated GLFW resources.
-    // ------------------------------------------------------------------
+  // glfw window creation
+  // --------------------
+  GLFWwindow *window =
+      glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+  if (window == NULL) {
+    std::cout << "Failed to create GLFW window" << std::endl;
     glfwTerminate();
-    return 0;
+    return -1;
+  }
+  glfwMakeContextCurrent(window);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+  // glad: load all OpenGL function pointers
+  // ---------------------------------------
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cout << "Failed to initialize GLAD" << std::endl;
+    return -1;
+  }
+  Shader shader1("shader.vs", "shader.fs"), shader2("shader.vs", "shader.fs2");
+
+  // set up vertex data (and buffer(s)) and configure vertex attributes
+  // ------------------------------------------------------------------
+  float firstTriangle[] = {
+      -0.5f, 0.0f,  0.0f, 1.0f, 0.0f, 0.0f, // left
+      0.0f,  -0.0f, 0.0f, 0.0f, 1.0f, 0.0f, // right
+      0.0f,  0.5f,  0.0f, 0.0f, 0.0f, 1.0f  // top
+  };
+  float secondTriangle[] = {
+      0.5f, -0.0f, 0.0f, 0.0f, 0.0f, 1.0f, // left
+       0.0f, -0.0f, 0.0f, 0.0f, 1.0f, 0.0f, // right
+      0.0f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f  // top
+  };
+
+  float texCoord[] = 
+  {
+    0.0f, 0.0f,
+    0.5f, 1.0f,
+    0.5f, 0.0f
+  };
+
+
+  unsigned int VBOs[2], VAOs[2];
+  glGenVertexArrays(2,VAOs); // we can also generate multiple VAOs or buffers at the same time
+  glGenBuffers(2, VBOs);
+  // first triangle setup
+  // --------------------
+  glBindVertexArray(VAOs[0]);
+  glBindBuffer(GL_ARRAY_BUFFER, VBOs[0]);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(firstTriangle), firstTriangle,
+               GL_STATIC_DRAW);
+
+  // le vertex pointer pour l'attribut aPos
+  //-------------------(location attribut, taille, --, --, stride, --)
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float),
+                        (void *)0); // Vertex attributes stay the same
+  glEnableVertexAttribArray(0);
+
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float),
+                        (void *)(3 * sizeof(float)));
+  glEnableVertexAttribArray(1);
+  // glBindVertexArray(0); // no need to unbind at all as we directly bind a
+  // different VAO the next few lines second triangle setup
+  // ---------------------
+  glBindVertexArray(VAOs[1]); // note that we bind to a different VAO now
+  glBindBuffer(GL_ARRAY_BUFFER, VBOs[1]); // and a different VBO
+  glBufferData(GL_ARRAY_BUFFER, sizeof(secondTriangle), secondTriangle,
+               GL_STATIC_DRAW);
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float),
+                        (void *)0); // Vertex attributes stay the same
+  glEnableVertexAttribArray(0);
+
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float),
+                        (void *)(3 * sizeof(float)));
+  glEnableVertexAttribArray(1);
+  // glBindVertexArray(0); // not really necessary as well, but beware of calls
+  // that could affect VAOs while this one is bound (like binding element buffer
+  // objects, or enabling/disabling vertex attributes)
+
+  // uncomment this call to draw in wireframe polygons.
+  // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+  // render loop
+  // -----------
+  while (!glfwWindowShouldClose(window)) {
+    // input
+    // -----
+    processInput(window);
+
+    // render
+    // ------
+    glClearColor(0.15f, 0.15f, 0.15f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // utilisation de "uniform" dans un shader GLSL
+
+    // float timevalue = glfwGetTime();
+    // float greenValue = (sin(timevalue) / 2.0f)  + 0.5f; // on veut modifier
+    // la valeur en controlant la valeur de G dans RGBA float redValue =
+    // (cos(timevalue) / 2.0f) + 0.5f; float blueValue = (acos(timevalue)
+    // / 2.0f) + 0.5f; Notons que la localisation de la valeur "uniform" e
+    // requiert pas l'utilisation du shader int vertexColorLocation =
+    // glGetUniformLocation(shaderProgram, "ourColor");
+
+    shader1.use();
+    shader1.setFloat("offset", 0.1f);
+
+    // Notons également que la MAJ d'un "uniform" se fait apres utilisation du
+    // shader (glUseProgram)
+    // glUniform4f(vertexColorLocation, redValue, greenValue, blueValue, 1.0f);
+    // draw first triangle using the data from the first VAO
+    glBindVertexArray(VAOs[0]);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    // then we draw the second triangle using the data from the second VAO
+
+    shader2.use();
+    glBindVertexArray(VAOs[1]);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved
+    // etc.)
+    // -------------------------------------------------------------------------------
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  // optional: de-allocate all resources once they've outlived their purpose:
+  // ------------------------------------------------------------------------
+  glDeleteVertexArrays(2, VAOs);
+  glDeleteBuffers(2, VBOs);
+
+  // glfw: terminate, clearing all previously allocated GLFW resources.
+  // ------------------------------------------------------------------
+  glfwTerminate();
+
+  return 0;
 }
 
-// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// process all input: query GLFW whether relevant keys are pressed/released this
+// frame and react accordingly
 // ---------------------------------------------------------------------------------------------------------
-void processInput(GLFWwindow* window)
-{
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
+void processInput(GLFWwindow *window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, true);
 }
 
-// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// glfw: whenever the window size changed (by OS or user resize) this callback
+// function executes
 // ---------------------------------------------------------------------------------------------
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-    // make sure the viewport matches the new window dimensions; note that width and 
-    // height will be significantly larger than specified on retina displays.
-    glViewport(0, 0, width, height);
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+  // make sure the viewport matches the new window dimensions; note that width
+  // and height will be significantly larger than specified on retina displays.
+  glViewport(0, 0, width, height);
 }
